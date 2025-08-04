07b7a5aec8da6ff214d7b70058bb25905168482c
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-04 07:26:43 +0430

    intailize player.

--- main.py
+++ main.py
@@ -13,3 +13,10 @@ COIN_COLOR = (255, 215, 0)
 PLAYER_COLOR = (0, 255, 127)        
 OBSTACLE_COLOR = (255, 76, 76)      
 TEXT_COLOR = (241, 245, 249)
+
+# intailze player
+player_width = 50
+player_height = 50
+player_x = screen_width // 2 - player_width // 2
+player_y = screen_height - player_height - 10
+player_speed = 5
