621d6bffb3713af1127ff887d6e85ab7b7bc3114
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-04 07:21:07 +0430

    intialize color.

--- main.py
+++ main.py
@@ -7,3 +7,9 @@ screen_width, screen_height = 800, 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Coin Collector-")
 
+# intialize color
+BACKGROUND_COLOR = (15, 23, 42)     
+COIN_COLOR = (255, 215, 0)           
+PLAYER_COLOR = (0, 255, 127)        
+OBSTACLE_COLOR = (255, 76, 76)      
+TEXT_COLOR = (241, 245, 249)
