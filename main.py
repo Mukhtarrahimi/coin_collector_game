f52cec173619acdbe1e0ecb10e0af44275619485
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-04 07:15:32 +0430

    intailize screen code.

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 import pygame
 import random
+
+# intialize screen
+pygame.init()
+screen_width, screen_height = 800, 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Coin Collector-")
+
